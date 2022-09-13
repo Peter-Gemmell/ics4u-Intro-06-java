c227c9a5dda222f32c11c0e7fbf992a51d41b8b3
Peter Gemmell <peter.gemmell@mths.ca~>
2022-09-13 00:10:27 +0000

    Hello

---
+++ HelloWorld.java
@@ -0,0 +1,40 @@
+/*
+* The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+*
+* @author  Mr Coxall
+* @version 1.0
+* @since   2020-01-01
+*/
+
+/**
+* This is the standard "Hello, World!" program.
+*/
+final class HelloWorld {
+
+    /**
+    * Prevent instantiation.
+    * Throw an exception IllegalStateException.
+    * if this ever is called
+    *
+    * @throws IllegalStateException
+    *
+    */
+    private HelloWorld() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+    public static void main(final String[] args) {
+
+        // print out "Hello, World!"
+        System.out.println("Hello, World!");
+
+        System.out.println("\nDone.");
+    }
+}
+
